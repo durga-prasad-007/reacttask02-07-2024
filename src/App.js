9bdfa19894be7b9f446ab5bc43b6eed169e90e45
Durga Prasad <dporange.siripuram007@gmail.com>
2024-07-21 02:02:29 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Image from './image';
 function App() {
   return (
     <>
     <h1>siripuram durga prasad</h1>
     <h2>html topics</h2>
     <p>html topics like table,list.......etc</p>
+    <Image/>
     </>
   );
 }
