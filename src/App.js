ac3e80491ea6a3ed79ab6cf07a6126ab5ebec195
Durga Prasad <dporange.siripuram007@gmail.com>
2024-07-20 14:38:49 +0530

    this is the first commir

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>siripuram durga prasad</h1>
+    <h2>html topics</h2>
+    <p>html topics like table,list.......etc</p>
+    </>
+  );
+}
+
+export default App;
