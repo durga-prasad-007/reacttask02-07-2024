5c82112124255f6791b2c43fcb38044585eb7bee
Durga Prasad <dporange.siripuram007@gmail.com>
2024-07-21 01:04:43 +0530

    list added

---
+++ src/list.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class List extends Component{
+    render(){
+        return(
+            <>
+            <h1>list:-</h1>
+            <ul>
+               <li>Coffee</li>
+               <li>Tea</li>
+               <li>Milk</li>
+               <li>sugar</li>
+               <li>lemon tea</li>
+               <li>green tea</li>
+               <li>black tea</li>
+            </ul>
+            </>
+        )
+    }
+}
+export default List
